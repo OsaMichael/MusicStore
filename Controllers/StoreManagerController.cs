e81e049b0f8b89f851b00c00e850a8204b4a9dd6
Your Name <Michael Aruebo>
2019-07-16 08:21:24 +0100

    initial

---
+++ Controllers/StoreManagerController.cs
@@ -0,0 +1,195 @@
+﻿using MvcMusikStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusikStore.Controllers
+{
+    //[Authorize(Roles = "Administrator")]
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        //
+        // GET: /StoreManager/
+
+        public ViewResult Index()
+        {
+            var albums = db.Albums.Include(a => a.Genre).Include(a => a.Artist);
+            return View(albums.ToList());
+        }
+
+        //
+        // GET: /StoreManager/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name");
+            return View();
+        }
+
+        //
+        // POST: /StoreManager/Create
+
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Album album = db.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        //
+        // GET: /StoreManager/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = db.Albums.Find(id);
+            db.Albums.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        //// GET: StoreManager
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //// GET: StoreManager/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: StoreManager/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: StoreManager/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: StoreManager/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: StoreManager/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: StoreManager/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: StoreManager/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
